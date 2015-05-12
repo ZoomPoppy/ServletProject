554dd11ab03c7db813291818990c5377bab2a56d
ZoomPoppy <514027735@qq.com>
2015-05-12 10:24:12 +0800

    Initial commit

---
+++ src/A.java
@@ -0,0 +1,32 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by zz on 2015/5/11.
+ */
+public class A extends HttpServlet  {
+    private String message;
+    public void init( )throws ServletException{
+        message = "hello world";
+    }
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException
+    {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy()
+    {
+        // 什么也不做
+    }
+}
